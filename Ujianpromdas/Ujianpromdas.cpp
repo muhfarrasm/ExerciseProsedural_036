33402c05d270c513ce2101e03ca0c0fd0ca8aa15
muhfarrasm <muhammadfarrasm@gmail.com>
2023-03-29 15:03:28 +0700

    int main

--- Ujianpromdas/Ujianpromdas.cpp
+++ Ujianpromdas/Ujianpromdas.cpp
@@ -14,3 +14,27 @@ string status(double r, double m) {
 		return "Ditolak";
 	}
 }
+
+int main() {
+	string nama[7];
+	int nilai_basa[7], nilai_mtk[7];
+	int jumlah_diterima = 0, jumlah_ditolak = 0;
+
+	for (int i = 0; i < 7; i++) {
+		cout << "Nama Mahasiswa Ke- " << (i + 1) << ": ";
+		cin >> nama[i];
+		cout << "Masukkan nilai matematika mahasiswa ke- " << i + 1 << ": ";
+		cin >> nilai_mtk[i];
+		cout << "Masukkan nilai bahasa inggris mahasiswa ke- " << i + 1 << ": ";
+		cin >> nilai_basa[i];
+
+		double rata_nilai = rata(nilai_mtk[i], nilai_basa[i]);
+		string status_mhs = status(rata(nilai_mtk[i], nilai_basa[i]), nilai_mtk[i]);
+
+		if (status_mhs == "Diterima") {
+			jumlah_diterima++;
+		}
+		else {
+			jumlah_ditolak++;
+		}
+	}
